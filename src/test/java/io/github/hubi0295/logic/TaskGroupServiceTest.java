c331526ac083eb05c538d20eb7d204300932daf6
= <nowa nazwa>
2024-02-16 00:16:19 +0100

    commit

--- src/test/java/io/github/hubi0295/logic/TaskGroupServiceTest.java
+++ src/test/java/io/github/hubi0295/logic/TaskGroupServiceTest.java
@@ -31,9 +31,6 @@ class TaskGroupServiceTest {
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("undone tasks");
     }
-
-
-
     @Test
     @DisplayName("should throw when no group")
     void toggleGroup_wrongId_throwsIllegalArgumentException() {
@@ -52,6 +49,7 @@ class TaskGroupServiceTest {
         assertThat(exception)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("id not found");
+
     }
 
     @Test
